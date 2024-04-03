eeb283d23e1e753a7b12e1edd0bd38ce155a96d4
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2024-04-03 17:49:10 +0200

    project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.snake {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.snake to javafx.fxml;
+    exports com.example.snake;
+}
